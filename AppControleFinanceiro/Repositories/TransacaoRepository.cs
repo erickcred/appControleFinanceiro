d78a639d6cb37cc9379587584dbcd9e997d2afc6
erickcred <erickcredjes@gmail.com>
2023-08-30 23:18:11 -0300

    Implementação Repository

--- AppControleFinanceiro/Repositories/TransacaoRepository.cs
+++ AppControleFinanceiro/Repositories/TransacaoRepository.cs
@@ -1,4 +1,6 @@
+using AppControleFinanceiro.Enums;
 using AppControleFinanceiro.Models;
+using LiteDB;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,20 +11,40 @@ namespace AppControleFinanceiro.Repositories
 {
   public class TransacaoRepository : ITransacaoRepository
   {
+    private LiteDatabase _context;
+    private readonly string collectionName = "Trasacoes";
     public TransacaoRepository()
-    { }
+    {
+      _context = new LiteDatabase("Filename=C:/database.db;Connection=Shared");
+    }
 
     public List<Transacao> ListarTransacaoes()
-    { return null; }
+    {
+      return _context.GetCollection<Transacao>(collectionName)
+                      .FindAll()
+                      .OrderByDescending(x => x.DataTransacao)
+                      .Where(x => x.Ativo == true && x.Situacao == ESituacao.Aberto)
+                      .ToList();
+    }
 
     public void CriarTransacao(Transacao transacao)
-    { }
+    {
+      _context.GetCollection<Transacao>(collectionName)
+                .Insert(transacao);
+    }
 
     public void AtualizarTransacao(Transacao transacao)
-    { }
+    {
+      _context.GetCollection<Transacao>(collectionName)
+                .Update(transacao);
+    }
 
     public void DeletarTransacao(Transacao transacao)
-    { }
+    {
+      transacao.Situacao = ESituacao.Deletada;
+      _context.GetCollection<Transacao>(collectionName)
+                .Update(transacao);
+    }
   }
 
 }
